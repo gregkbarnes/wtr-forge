109b8f0d9db2bff7b48fd6043a72e4cb183f4170
Greg Barnes <gregkbarnes@gmail.com>
2023-09-05 20:03:23 -0700

    freeze accepts parameter

--- script/wtrToken.freeze.sh
+++ script/wtrToken.freeze.sh
@@ -8,7 +8,7 @@ if [ "$#" -eq 0 ]; then
     echo "error"
     exit 1
 elif [ "$#" -eq 1 ]; then
-    cast send $CONTRACT_WTRTOKEN "freeze(address)()" "0x4dA5095C9D2e8c802dEA08A03dA0C7c6810D36c7" --rpc-url $RPC_URL_LOCAL --private-key $PK_PAUSE
+    cast send $CONTRACT_WTRTOKEN "freeze(address)()" $1 --rpc-url $RPC_URL_LOCAL --private-key $PK_PAUSE
 elif [ "$#" -eq 2 ]; then
     echo "error"
     exit 1
